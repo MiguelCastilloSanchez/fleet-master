6f2af1c3f81b9d0e60ff704251ef450e0111d1bb
Jean Buenfil <jeanbuenfil.jb@gmail.com>
2025-05-28 17:41:25 -0600

    FM-29 Fix: fixed assignment integration with front-end

--- src/main/java/com/example/fleetmaster/infrastructure/assignment/controller/CreateAssignmentController.java
+++ src/main/java/com/example/fleetmaster/infrastructure/assignment/controller/CreateAssignmentController.java
@@ -2,6 +2,7 @@ package com.example.fleetmaster.infrastructure.assignment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,7 +13,7 @@ import com.example.fleetmaster.infrastructure.assignment.dto.AssignmentPublicDat
 import com.example.fleetmaster.usecase.assignment.CreateAssignmentUseCase;
 
 import jakarta.validation.Valid;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class CreateAssignmentController {
 
--- src/main/java/com/example/fleetmaster/infrastructure/assignment/controller/SearchAssignmentController.java
+++ src/main/java/com/example/fleetmaster/infrastructure/assignment/controller/SearchAssignmentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.example.fleetmaster.entity.assigment.model.Assignment;
 import com.example.fleetmaster.infrastructure.assignment.dto.AssignmentPublicData;
 import com.example.fleetmaster.usecase.assignment.SearchAssignmentUseCase;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class SearchAssignmentController {
 
